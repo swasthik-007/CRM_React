206e5771686d5361d791a21c9ad929d6ba36b49b
swasthik-007 <mohanty.swastik4@gmail.com>
2024-05-27 21:41:05 +0530

    Added our first async thunk for login

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -5,6 +5,7 @@ const store = configureStore({
     reducer: {
         auth: authSliceReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
 });
 
--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,9 +1,7 @@
 import axios from "axios";
+const axiosInstance = axios.create();
 
-const axiosInstance=axios.create();
-
-axiosInstance.defaults.baseURL=import.meta.env.VITE_BASE_URL;
-axiosInstance.defaults.withCredentials=true;
-axiosInstance.defaults.timeout=import.meta.env.VITE_API_TIMEOUT;
+axiosInstance.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axiosInstance.defaults.timeout = import.meta.env.VITE_API_TIMEOUT;
 
 export default axiosInstance;
